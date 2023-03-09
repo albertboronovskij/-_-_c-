d0812244423fe54f225032bc15d29e69739aab2d
albertboronovskij <albertikus1010@gmail.com>
2023-03-09 19:30:01 +0700

    init commit

---
+++ ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Console.WriteLine("введи цифру дня недели ");
+int a=3;
+
+ if (a<8)
+ {
+if (a==1)
+{
+    Console.WriteLine("понедельник");
+}
+if (a==2)
+{
+    Console.WriteLine("вторник");
+}
+if (a==3)
+{
+    Console.WriteLine("среда");
+}
+if (a==4)
+{
+    Console.WriteLine("четверг");
+}
+if (a==5)
+{
+    Console.WriteLine("пятница");
+}
+if (a==6)
+{
+    Console.WriteLine("суббота");
+}
+if (a==7)
+{
+    Console.WriteLine("воскресенье");
+}
+ }
+ else Console.WriteLine("это не день недели дурик ");
+//понедельник вторник среда четверг пятница суббота воскресенье
+
+
---
+++ заданние 1 /Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 2;
+int b =4;
+if (a*a==b){
+    Console.WriteLine("yes");
+}
+else
+{
+     Console.WriteLine("no");
+
+}
